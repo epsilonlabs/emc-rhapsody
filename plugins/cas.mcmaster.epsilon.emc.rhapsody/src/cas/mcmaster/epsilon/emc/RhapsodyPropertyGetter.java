4abfaddb2ec3c8cb10e8810d15670eaa4cacc9f2
hoyosroh <hoyosroh@vd-hoyosroh.cas.mcmaster.ca>
2023-05-25 14:53:47 -0600

    Fixes in the property getter

--- plugins/cas.mcmaster.epsilon.emc.rhapsody/src/cas/mcmaster/epsilon/emc/RhapsodyPropertyGetter.java
+++ plugins/cas.mcmaster.epsilon.emc.rhapsody/src/cas/mcmaster/epsilon/emc/RhapsodyPropertyGetter.java
@@ -4,48 +4,80 @@ import java.util.ArrayList;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.eclipse.epsilon.common.module.ModuleElement;
+import org.eclipse.epsilon.eol.exceptions.EolIllegalPropertyException;
 import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
 import org.eclipse.epsilon.eol.execute.context.IEolContext;
-import org.eclipse.epsilon.eol.execute.introspection.java.JavaPropertyGetter;
+import org.eclipse.epsilon.eol.execute.introspection.IPropertyGetter;
+import org.eclipse.epsilon.eol.execute.introspection.java.ObjectMethod;
+import org.eclipse.epsilon.eol.execute.operations.contributors.OperationContributorRegistry;
 
 import com.telelogic.rhapsody.core.IRPInstanceValue;
 import com.telelogic.rhapsody.core.IRPLiteralSpecification;
 import com.telelogic.rhapsody.core.IRPModelElement;
 import com.telelogic.rhapsody.core.IRPTag;
 
-public class RhapsodyPropertyGetter extends JavaPropertyGetter {
+public class RhapsodyPropertyGetter implements IPropertyGetter {
 
 	@Override
-	public Object invoke(Object object, String property, IEolContext context) throws EolRuntimeException {
-		if (!(object instanceof IRPModelElement)) {
+	public boolean hasProperty(Object object, String property, IEolContext context) {
+		try (ObjectMethod om = getMethodFor(object, property, context)) {
+			if (om.getMethod() != null) {
+				return true;
+			}
+		}
+		// Can be a stereotype tag?
+		IRPModelElement element = (IRPModelElement) object;
+		return element.getTag(property) != null;
+	}
+	
+	@Override
+	public Object invoke(
+		Object target,
+		String property,
+		IEolContext context) throws EolRuntimeException {
+		if (!(target instanceof IRPModelElement)) {
 			throw new IllegalArgumentException("Can't get ptoperty of none IRPModelElement");
 		}
-		try {
-			return super.invoke(object, property, context);
-		} catch (EolRuntimeException e) {
-			// Can be a stereotype tag?
-			IRPModelElement element = (IRPModelElement) object;
-			IRPTag tag = element.getTag(property);
-			var valSpecs = tag.getValueSpecifications();
-			var looper = valSpecs.toList().iterator();
-			var result = new ArrayList<Object>();
-			while(looper.hasNext()) {
-				var valSpec = looper.next();
-				if (valSpec instanceof IRPInstanceValue) {
-					result.add(((IRPInstanceValue)valSpec).getValue());
-				} else if (valSpec instanceof IRPLiteralSpecification) {
-					result.add( getLiteralSpecValue(tag, (IRPLiteralSpecification) valSpec));
-				} else {
-					throw new IllegalStateException("Unknown value specification type: " + valSpec.getClass().getName());
-				}
-			}
-			if (result.size() == 1) {
-				return result.get(0);
-			} else {
-				return result;
+		try (ObjectMethod objectMethod = getMethodFor(target, property, context)) {
+			if (objectMethod.getMethod() != null) {
+				ModuleElement ast = context.getExecutorFactory().getActiveModuleElement();
+				return objectMethod.execute(ast, context);
 			}
 		}
+		// Can be a stereotype tag?
+		IRPModelElement element = (IRPModelElement) target;
+		IRPTag tag = element.getTag(property);
+		if (tag == null) {
+			LOG.error("Could not find a property or tag with name {}", property);
+			throw new EolIllegalPropertyException(
+					target,
+					property,
+					context.getExecutorFactory().getActiveModuleElement(),
+					context);
+		}
+		var valSpecs = tag.getValueSpecifications();
+		var looper = valSpecs.toList().iterator();
+		var result = new ArrayList<Object>();
+		while(looper.hasNext()) {
+			var valSpec = looper.next();
+			if (valSpec instanceof IRPInstanceValue) {
+				result.add(((IRPInstanceValue)valSpec).getValue());
+			} else if (valSpec instanceof IRPLiteralSpecification) {
+				result.add( getLiteralSpecValue(tag, (IRPLiteralSpecification) valSpec));
+			} else {
+				throw new IllegalStateException("Unknown value specification type: " + valSpec.getClass().getName());
+			}
+		}
+		if (result.size() == 1) {
+			return result.get(0);
+		} else {
+			return result;
+		}
+		
 	}
+	
+	private static final Logger LOG = LogManager.getLogger(RhapsodyPropertyGetter.class);
 
 	private Object getLiteralSpecValue(IRPTag tag, IRPLiteralSpecification valSpec) {
 		var valSpecVal = valSpec.getValue();
@@ -76,6 +108,26 @@ public class RhapsodyPropertyGetter extends JavaPropertyGetter {
 		return result;
 	}
 	
-	private static final Logger LOG = LogManager.getLogger(RhapsodyPropertyGetter.class);
+	private ObjectMethod getMethodFor(Object object, String property, IEolContext context) {
+		OperationContributorRegistry registry = context.getOperationContributorRegistry();
+		
+		// Look for an X() method
+		ObjectMethod om = registry.findContributedMethodForEvaluatedParameters(object, property, new Object[]{}, context);
+		if (om != null) return om;
+				
+		// Look for a getX() method
+		om = registry.findContributedMethodForEvaluatedParameters(object, "get" + property, new Object[]{}, context);
+		if (om != null) return om;
+
+		// Look for an isX() method
+		om = registry.findContributedMethodForEvaluatedParameters(object, "is" + property, new Object[]{}, context);
+		if (om != null) return om;
+		
+		// Look for an hasX() method
+		om = registry.findContributedMethodForEvaluatedParameters(object, "has" + property, new Object[]{}, context);
+		if (om != null) return om;
+		
+		return new ObjectMethod(object);
+	}
 
 }
--- tests/cas.mcmaster.epsilon.emc.rhapsody.test.unit/src/cas/mcmaster/tests/RhapsodyPropertyGetterTests.java
+++ tests/cas.mcmaster.epsilon.emc.rhapsody.test.unit/src/cas/mcmaster/tests/RhapsodyPropertyGetterTests.java
@@ -1,6 +1,7 @@
 package cas.mcmaster.tests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
@@ -8,6 +9,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
+import org.eclipse.epsilon.eol.exceptions.EolIllegalPropertyException;
 import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
 import org.eclipse.epsilon.eol.execute.context.EolContext;
 import org.junit.jupiter.api.AfterAll;
@@ -41,22 +43,52 @@ public class RhapsodyPropertyGetterTests {
 	
 	@AfterAll
 	static void unload() {
-//		if (prj != null) {
-//			prj.close();					
-//		}
+		if (prj != null) {
+			prj.close();					
+		}
 		if(!rhapsodyWasActive && (app != null)) {
 			app.quit();
 		}
 	}
 	
-	@Test
-	void get_property_for_api() {
-		var block = prj.findNestedElementRecursive("Ambulance", "Vehicle");
+	@ParameterizedTest
+	@MethodSource
+	void has_property(String property, boolean expected) {
+		var block = prj.findNestedElementRecursive("Test", "Block");
 		if (block == null) {
-			fail("Vehicle with name 'Ambulance' should exist");
+			fail("Block with name 'Camera' should exist");
 		}
-		var blocks = prj.getNestedElementsByMetaClass("Vehicle", 1);
-		System.out.println(blocks);
+		var underTest = new RhapsodyPropertyGetter();
+		assertEquals(expected, underTest.hasProperty(block, property, new EolContext()));
+	}
+	
+	@Test
+	void get_unknown_property_throws() {
+		var block = prj.findNestedElementRecursive("Camera", "Block");
+		if (block == null) {
+			fail("Block with name 'Camera' should exist");
+		}
+		var underTest = new RhapsodyPropertyGetter();
+		var context = new EolContext();
+		assertThrows(EolIllegalPropertyException.class, () -> {
+			underTest.invoke(block, "film", context);
+		});
+	}
+	
+	@ParameterizedTest
+	@MethodSource
+	void get_property_from_api(String property, Object expected) {
+		var block = prj.findNestedElementRecursive("Camera", "Block");
+		if (block == null) {
+			fail("Block with name 'Camera' should exist");
+		}
+		var underTest = new RhapsodyPropertyGetter();
+		try {
+			var value = underTest.invoke(block, property, new EolContext());
+			assertEquals(expected, value);
+		} catch (EolRuntimeException e) {
+			fail("Property should exist");
+		}
 	}
 	
 	@ParameterizedTest
@@ -67,13 +99,36 @@ public class RhapsodyPropertyGetterTests {
 			fail("Block with name 'Test' should exist");
 		}
 		var underTest = new RhapsodyPropertyGetter();
-		var context = new EolContext();
 		try {
-			var value = underTest.invoke(block, tagName, context);
+			var value = underTest.invoke(block, tagName, new EolContext());
 			assertEquals(expected, value);
 		} catch (EolRuntimeException e) {
-			fail("Property 'name' should exist");
+			fail("Property should exist");
 		}
+	}
+	
+	/**
+	 * One getX, one isX, one hasX, one tag, one invalid
+	 */
+	static Stream<Arguments> has_property() {
+		return Stream.of(
+		        arguments("name", true),
+		        arguments("readOnly", true),
+		        arguments("hasNestedElements", true),
+		        arguments("boolVal", true),
+		        arguments("planet", false)
+		    );
+	}
+	
+	/**
+	 * One getX, one isX, one hasX
+	 */
+	static Stream<Arguments> get_property_from_api() {
+		return Stream.of(
+		        arguments("name", "Camera"),
+		        arguments("readOnly", 0),
+		        arguments("hasNestedElements", 1)		        
+		    );
 	}
 	
 	static Stream<Arguments> get_property_from_tag() {
