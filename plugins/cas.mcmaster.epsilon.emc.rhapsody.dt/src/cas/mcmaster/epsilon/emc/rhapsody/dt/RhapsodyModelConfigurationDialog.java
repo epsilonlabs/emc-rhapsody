3d9ee4aa39d6daac13fbd133d4ad9ad04f5fe2d1
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-07-04 10:13:37 -0400

    Completed and Functional Config UI
    Added all suggested buttons and text fields to store main package name,
    whether the selected element will be the root, where the Rhapsody
    installation is located, and selecting the model file from the
    workspace.

--- plugins/cas.mcmaster.epsilon.emc.rhapsody.dt/src/cas/mcmaster/epsilon/emc/rhapsody/dt/RhapsodyModelConfigurationDialog.java
+++ plugins/cas.mcmaster.epsilon.emc.rhapsody.dt/src/cas/mcmaster/epsilon/emc/rhapsody/dt/RhapsodyModelConfigurationDialog.java
@@ -7,11 +7,12 @@ import org.eclipse.swt.events.*;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.*;
 
+import cas.mcmaster.epsilon.emc.RhapsodyModel;
+
 public class RhapsodyModelConfigurationDialog extends AbstractCachedModelConfigurationDialog {
 
-	private Text fileText;
-	private boolean selectElemRoot;
-	private Text rhapsodyText;
+	private Text fileText, rhapsodyText, mainPackageText;
+	private Button setRootElemButton;
 
 	@Override
 	protected String getModelName() {
@@ -30,7 +31,7 @@ public class RhapsodyModelConfigurationDialog extends AbstractCachedModelConfigu
 		super.createGroups(control);
 		createFileGroup(control);
 		createLoadStoreOptionsGroup(control);
-		createRhapsodyInstallationGroup(control);
+		createRhapsodyGroup(control);
 	}
 
 	private void createFileGroup(Composite parent) {
@@ -40,65 +41,74 @@ public class RhapsodyModelConfigurationDialog extends AbstractCachedModelConfigu
 		modelFileLabel.setText("Model file: ");
 
 		fileText = new Text(groupContent, SWT.BORDER);
-		fileText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-
+		fileText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
 		final Button browseFile = new Button(groupContent, SWT.NONE);
 		browseFile.setText("Browse Workspace...");
 		browseFile.addListener(SWT.Selection, new BrowseWorkspaceForModelsListener(fileText, "Rhapsody model files in the workspace",
-				"Select a Rhapsody model file"));
-		final Label useCurrentProjLabel = new Label(groupContent, SWT.NONE);
-		useCurrentProjLabel.setText("Use current project: ");
-		final Button useCurrentProjectCheckBtn = new Button(groupContent, SWT.CHECK);
-		useCurrentProjectCheckBtn.addSelectionListener(new CurrentProjectListener(fileText));
+				"Select a Rhapsody project file"));
+		/*
+		 * final Label useCurrentProjLabel = new Label(groupContent, SWT.NONE);
+		 * useCurrentProjLabel.setText("Use current project: "); final Button
+		 * useCurrentProjectCheckBtn = new Button(groupContent, SWT.CHECK);
+		 * useCurrentProjectCheckBtn.addSelectionListener(new
+		 * CurrentProjectListener(fileText));
+		 */
 	}
 
-	private void createRhapsodyInstallationGroup(Composite parent) {
-		final Composite groupContent = DialogUtil.createGroupContainer(parent, "Rhapsody Installation", 3);
+	private void createRhapsodyGroup(Composite parent) {
+		final Composite groupContent = DialogUtil.createGroupContainer(parent, "Rhapsody", 3);
 
-		final Label modelFileLabel = new Label(groupContent, SWT.NONE);
-		modelFileLabel.setText("Rhapsody installation: ");
-
+		final Label installationDirLabel = new Label(groupContent, SWT.NONE);
+		installationDirLabel.setText("Rhapsody installation directory: ");
+		installationDirLabel.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
 		rhapsodyText = new Text(groupContent, SWT.BORDER);
-		rhapsodyText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		rhapsodyText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
 
 		final Button browseFile = new Button(groupContent, SWT.NONE);
 		browseFile.setText("Browse...");
 		browseFile.addSelectionListener(new SelectionListener() {
 
+			@Override
 			public void widgetDefaultSelected(SelectionEvent e) {
 			}
 
+			@Override
 			public void widgetSelected(SelectionEvent e) {
-				FileDialog dlg = new FileDialog(browseFile.getShell(),  SWT.OPEN);
-				dlg.setText("Open");
+				DirectoryDialog dlg = new DirectoryDialog(browseFile.getShell(),  SWT.OPEN);
+				dlg.setText("Select Rhapsody Installation Folder");
 				String path = dlg.open();
 				if (path == null) return;
 				rhapsodyText.setText(path);
 			}
 		});
+		final Label mainPackageNameLabel = new Label(groupContent, SWT.NONE);
+		mainPackageNameLabel.setText("Main package name: ");
+		mainPackageText = new Text(groupContent, SWT.BORDER);
+		mainPackageText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
+		setRootElemButton = new Button(groupContent, SWT.CHECK);
+		setRootElemButton.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false, 3, 1));
+		setRootElemButton.setText("Selected element as root");
+	}
+	
+	@Override
+	protected void loadProperties() {
+		super.loadProperties();
+		if (properties == null) {
+			return;
+		}
+		setRootElemButton.setSelection(Boolean.parseBoolean(properties.getProperty(RhapsodyModel.PROPERTY_ROOT_ELEM)));
+		rhapsodyText.setText(properties.getProperty(RhapsodyModel.PROPERTY_INSTALLATION_DIRECTORY));
+		fileText.setText(properties.getProperty(RhapsodyModel.PROPERTY_PROJECT_PATH));
+		mainPackageText.setText(properties.getProperty(RhapsodyModel.PROPERTY_MAIN_PACKAGE_NAME));
 	}
 
-	protected class CurrentProjectListener extends SelectionAdapter {
-
-		private Text targetText;
-
-		public CurrentProjectListener(Text target) {
-			targetText = target;
-		}
-
-		@Override
-		public void widgetSelected(SelectionEvent event) {
-			if (((Button) event.getSource()).getSelection()) {
-				targetText.setText("current project");
-			}
-		}
+	@Override
+	public void storeProperties() {
+		super.storeProperties();
+		properties.setProperty(RhapsodyModel.PROPERTY_PROJECT_PATH, fileText.getText());
+		properties.setProperty(RhapsodyModel.PROPERTY_INSTALLATION_DIRECTORY, rhapsodyText.getText());
+		properties.setProperty(RhapsodyModel.PROPERTY_ROOT_ELEM, String.valueOf(setRootElemButton.getSelection()));
+		properties.setProperty(RhapsodyModel.PROPERTY_MAIN_PACKAGE_NAME, mainPackageText.getText());
 	}
-
-	protected class RootElementListener extends SelectionAdapter {
-
-		@Override
-		public void widgetSelected(SelectionEvent event) {
-			selectElemRoot = ((Button) event.getSource()).getSelection();
-		}
-	}
+	
 }
